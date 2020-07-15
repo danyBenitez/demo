58b8d696670dc138c1ca13cf366041d1dd5ff967
danyBenitez <dannydaniel021693@gmail.com>
2020-07-15 10:09:17 -0500

    prueba de commit

--- src/main/java/com/demo/auth/ResourceServerConfig.java
+++ src/main/java/com/demo/auth/ResourceServerConfig.java
@@ -19,7 +19,7 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{  
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
